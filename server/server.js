e784ca596e4f5102480d1ac75f9b9417a5e22f7b
Akshat Khare <akshat1514@gmail.com>
2023-12-10 21:37:20 -0500

    Data from fork 4

--- server/server.js
+++ server/server.js
@@ -2,10 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const DatabaseSingleton = require("./utils/dbConnection");
-const session = require('express-session')
-const store = new session.MemoryStore();
-
-const Users = require("./models/users")
+const session = require('express-session');
+const csrf = require('csurf');
+const Users = require("./models/users");
 
 // routes
 const questionRoute = require("./routes/questions");
@@ -25,7 +24,17 @@ app.use(session({
     cookie: { maxAge: 30000 },
     saveUninitialized: false,
     resave: false
-}))
+}));
+
+// CSRF middleware
+const csrfProtection = csrf({ cookie: true });
+app.use(csrfProtection);
+
+// Custom middleware to set CSRF token in locals
+app.use((req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 // ========= ENV VARIABLES =========
 const PORT = process.env.PORT || 8000;
@@ -41,6 +50,19 @@ app.use("/tags", tagsRoute);
 app.use("/user", userRoute);
 app.use("/", profileRoute);
 app.use("/", commentsRoute);
+
+app.get('/questions', (req, res) => {
+  res.json({ csrfToken: res.locals.csrfToken });
+});
+
+// Error handling middleware for CSRF errors
+app.use((err, req, res, next) => {
+  if (err.code === 'EBADCSRFTOKEN') {
+    res.status(403).json({ error: 'Invalid CSRF token' });
+  } else {
+    next(err);
+  }
+});
 
 // Handle server termination
 const server = app.listen(PORT, () => {
